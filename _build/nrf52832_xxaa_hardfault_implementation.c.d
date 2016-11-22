c4989e044e66a874c2f7b812835198a392c82744
Northshoot <laurynas.riliskis@gmail.com>
2016-11-22 13:16:17 -0800

    removed garbadge

--- _build/nrf52832_xxaa_hardfault_implementation.c.d
+++
@@ -1,5 +0,0 @@
-_build/nrf52832_xxaa_hardfault_implementation.c.o: \
- /Users/lauril/workspace/12-sdk/nRF5_SDK_12.0.0/components/libraries/hardfault/hardfault_implementation.c \
- config/sdk_config.h
-
-config/sdk_config.h:
